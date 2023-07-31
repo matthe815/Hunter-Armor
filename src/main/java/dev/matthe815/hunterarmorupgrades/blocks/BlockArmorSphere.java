16d270945d31cab76094dd62f9ccb128024edee0
Matthe815 <matthew.elswick@matthe815.dev>
2023-07-31 16:15:19 -0400

    block: Added armor sphere as a drop form ore

--- src/main/java/dev/matthe815/hunterarmorupgrades/blocks/BlockArmorSphere.java
+++ src/main/java/dev/matthe815/hunterarmorupgrades/blocks/BlockArmorSphere.java
@@ -1,6 +1,7 @@
 package dev.matthe815.hunterarmorupgrades.blocks;
 
 import dev.matthe815.hunterarmorupgrades.HunterArmorUpgrades;
+import dev.matthe815.hunterarmorupgrades.Registration;
 import dev.matthe815.hunterarmorupgrades.containers.ContainerArmorCrafter;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -10,6 +11,8 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootContext;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ResourceLocation;
@@ -17,19 +20,27 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class BlockArmorSphere extends Block {
     public BlockArmorSphere() {
         super(Properties.create(Material.IRON));
         this.setRegistryName(new ResourceLocation(HunterArmorUpgrades.MOD_ID, "armor_sphere_ore"));
+    }
 
+    @Override
+    public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+        return Arrays.asList(new ItemStack[] { new ItemStack(Registration.ARMOR_SPHERE) });
     }
 
     public static class ItemBlockArmorCrafter extends BlockItem {
         public ItemBlockArmorCrafter(Block blockIn) {
-            super(blockIn, new Properties());
+            super(blockIn, new Properties().group(Registration.HUNTER_ARMOR));
             this.setRegistryName(new ResourceLocation(HunterArmorUpgrades.MOD_ID, "armor_sphere_ore"));
         }
     }
